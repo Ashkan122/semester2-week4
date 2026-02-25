e2d661d3a76eb08bc60f711031baa4101e5b977b
Ashkan Ramjerdi <xcqc0000@leeds.ac.uk>
2026-02-25 17:01:30 +0000

    gfggggiiiiiiiiigs

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -13,9 +13,8 @@ int main( int argc, char **argv ) {
     char Strings[100] = ""; 
     if (argc <=11) {
         for( int k=1; k<argc; ++k ){
-            //remove any arithmetic operators from argv[k] and then check if the length of the resulting string is between 1 and 4 (inclusive). If it is, concatenate it to Strings, separating each valid string with a hyphen ('-').
             
-            if (strlen(argv[k])<=4 && strlen(argv[k])>=1 && (strchr(argv[k], '+') == NULL ) ){
+            if (strlen(argv[k])<=4 && strlen(argv[k])>=1 || (strchr(argv[k], '+') != NULL ) ){
                 strcat(Strings, argv[k]);
                 if (k<argc-1) {
                     strcat(Strings, "-");
